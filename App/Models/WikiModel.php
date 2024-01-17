091dd6b41063d687d7b683c744ffa57192bf9581
“[Khalid <[khalid.haiddou01@gmail.com]>
2024-01-17 16:39:24 +0100

    modify the search with ajax

--- App/Models/WikiModel.php
+++ App/Models/WikiModel.php
@@ -26,34 +26,23 @@ class WikiModel {
     
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    // public function getAllWikis($inp) {
-    //     $conn = $this->db->getConnection();
-    
-    //     $stmt = $conn->prepare(
-    //         "SELECT
-    //         * from wiki where title=?"
-    //         );
-    //     $stmt->execute([$inp]);
-    
-    //     return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    // }
-
-
-    
+   
 
     public function Search_wiki($text){
         $conn = $this->db->getConnection();
-        $stmt = $conn->prepare(
-            "SELECT w.*, u.username as author, c.name as category FROM wiki w 
-            LEFT JOIN users u ON w.authorID = u.id
-            LEFT JOIN categories c ON w.categorieID = c.id
-            WHERE w.title LIKE :title 
-            OR c.name LIKE :category;" 
-            );
-        $stmt->bindParam(":title", $text, PDO::PARAM_STR);
-        $stmt->bindParam(":category", $text, PDO::PARAM_STR);
+        $stmt = $conn->prepare (
+         
+            "SELECT * FROM wiki JOIN categories ON wiki.categorieID = categories.id WHERE wiki.title LIKE :title OR categories.name LIKE :category;" 
+         
+        );   
+        $var1 = "%".$text."%";
+        $stmt->bindParam(":title", $var1, PDO::PARAM_STR);
+        $stmt->bindParam(":category", $var1, PDO::PARAM_STR);
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $result;
     }
 }
+
+
+//bindparam 
